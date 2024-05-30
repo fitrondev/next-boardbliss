2a2ee5fe1ee6c521568b51a3ee50ea513ca2a1a0
fitronansori <fitronansori7@gmail.com>
2024-05-30 17:01:10 +0800

    feat: Add organization ID page component

---
+++ src/app/(dashboard)/organization/[organizationId]/page.tsx
@@ -0,0 +1,10 @@
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const OrganizationIdPage = () => {
+  return (
+    <div>
+      <OrganizationSwitcher hidePersonal />
+    </div>
+  );
+};
+export default OrganizationIdPage;
