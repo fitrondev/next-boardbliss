2db611142b05f415bd206d20f30171226aa9134f
fitronansori <fitronansori7@gmail.com>
2024-05-30 17:00:26 +0800

    feat: Add sign-in and sign-up pages

---
+++ src/middleware.ts
@@ -0,0 +1,15 @@
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/organization(.*)",
+  "/select-org",
+]);
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
+});
+
+export const config = {
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+};
