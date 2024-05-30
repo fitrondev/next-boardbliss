d6e71ddc7e473b45e9a5172f2a2d559e12568db4
fitronansori <fitronansori7@gmail.com>
2024-05-30 17:00:55 +0800

    feat: Add DashNavbar component

---
+++ src/app/(dashboard)/_components/DashNavbar/index.tsx
@@ -0,0 +1,35 @@
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+
+import Logo from "@/components/Logo";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const DashNavbar = () => {
+  return (
+    <header className="shadow-sm">
+      <div className="h-16 container flex items-center justify-between">
+        <div className="inline-flex items-center gap-2">
+          <Logo />
+
+          <Button size={"sm"} asChild>
+            <Link href={"/select-org"}>Create</Link>
+          </Button>
+        </div>
+
+        <div>
+          <div className="inline-flex items-center gap-2">
+            <OrganizationSwitcher
+              hidePersonal
+              afterCreateOrganizationUrl={"/organization/:id"}
+              afterLeaveOrganizationUrl="/select-org"
+              afterSelectOrganizationUrl={"/organization/:id"}
+            />
+
+            <UserButton />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+export default DashNavbar;
